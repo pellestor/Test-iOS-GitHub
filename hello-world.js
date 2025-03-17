eddf05d0e04b48069ba301ee46ba99d40cb005c0
pellestor <94915656+pellestor@users.noreply.github.com>
2025-03-17 16:20:12 -0400

    Update and rename hello-world.yml to hello-world.js

---
+++ hello-world.js
@@ -0,0 +1,3 @@
+// This script generates a URL to open Safari and display "Hello, World!" on a web page
+let url = "data:text/html,<html><body><h1>Hello, World!</h1></body></html>";
+Safari.openURL(url);
